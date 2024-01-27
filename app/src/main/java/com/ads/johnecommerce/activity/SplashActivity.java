9b44e8f93941d4ebb2760772e24fc35728cd7ae6
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-01-26 23:35:34 -0300

    Iniciando Projeto

---
+++ app/src/main/java/com/ads/johnecommerce/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.ads.johnecommerce.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.ads.johnecommerce.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(getMainLooper()).postDelayed(() ->{
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+
+        }, 4000);
+    }
+
+}
