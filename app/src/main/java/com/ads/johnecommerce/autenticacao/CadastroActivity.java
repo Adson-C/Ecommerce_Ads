e9e4a5069391f8f368bdc7491a83d1348205a6a5
Adson-C <adsonconcecao@yahoo.com.br>
2024-02-06 18:52:42 -0300

    Criando Activity de Cadastro/ FirebaseHelper

--- app/src/main/java/com/ads/johnecommerce/autenticacao/CadastroActivity.java
+++ app/src/main/java/com/ads/johnecommerce/autenticacao/CadastroActivity.java
@@ -3,9 +3,13 @@ package com.ads.johnecommerce.autenticacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ads.johnecommerce.R;
 import com.ads.johnecommerce.databinding.ActivityCadastroBinding;
+import com.ads.johnecommerce.helper.FirebaseHelper;
+import com.ads.johnecommerce.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -19,6 +23,77 @@ public class CadastroActivity extends AppCompatActivity {
 
         configClicks();
     }
+
+    public void validaDados(View view) {
+
+        String nome = binding.edtNome.getText().toString().trim();
+        String email = binding.edtEmal.getText().toString().trim();
+        String senha = binding.edtSena.getText().toString().trim();
+        String confirmasenha = binding.edtSenConfirma.getText().toString().trim();
+
+        if (!nome.isEmpty()){
+            if (!email.isEmpty()){
+                if (!senha.isEmpty()){
+                    if (!confirmasenha.isEmpty()){
+                        if (senha.equals(confirmasenha)){
+
+                            binding.progressBarCriar.setVisibility(View.VISIBLE);
+
+                            Usuario usuario = new Usuario();
+                                usuario.setNome(nome);
+                                usuario.setEmail(email);
+                                usuario.setSenha(senha);
+
+                                criarConta(usuario);
+
+                        }else {
+                            binding.edtSenConfirma.requestFocus();
+                            binding.edtSenConfirma.setError("Senhas não conferi.");
+                        }
+
+                    }else {
+                        binding.edtSenConfirma.requestFocus();
+                        binding.edtSenConfirma.setError("Confirme sua senha.");
+                    }
+
+                }else {
+                    binding.edtSena.requestFocus();
+                    binding.edtSena.setError("Informe sua senha.");
+                }
+
+            }else {
+                binding.edtEmal.requestFocus();
+                binding.edtEmal.setError("Informe seu E-mail.");
+            }
+        }else {
+            binding.edtNome.requestFocus();
+            binding.edtNome.setError("Informe seu nome.");
+        }
+    }
+
+    private void criarConta(Usuario usuario){
+        FirebaseHelper.getAuth().createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                // chamando metodo para slavar o id do usuario
+                String id = task.getResult().getUser().getUid();
+
+                usuario.setId(id);
+                // metoto invocado da classe Usuario
+                usuario.salvar();
+
+            }else {
+                // chamando a class FirebaseHelper --> validaErros no TOAST
+                Toast.makeText(this, FirebaseHelper.validaErros(task.getException().getMessage()), Toast.LENGTH_SHORT).show();
+
+                }
+
+            binding.progressBarCriar.setVisibility(View.GONE);
+        });
+
+    }
+
 
     private void configClicks() {
 
---
+++ app/src/main/java/com/ads/johnecommerce/model/Usuario.java
@@ -0,0 +1,61 @@
+package com.ads.johnecommerce.model;
+
+import com.ads.johnecommerce.helper.FirebaseHelper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+    private String id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(){
+
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Exclude
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    // metodo para salvar usuario no FirebaseHelper
+    public void salvar() {
+        DatabaseReference usuariRef = FirebaseHelper.getDatabaseReference()
+                .child("usuarios")
+                .child(this.getId());
+        usuariRef.setValue(this);
+
+    }
+}
